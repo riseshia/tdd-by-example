def94bd78903b1edce1431a7a7e757433ea92457
Shia <rise.shia@gmail.com>
2017-05-05 15:39:31 +0900

    Add factory method

--- money.rb
+++ money.rb
@@ -5,12 +5,20 @@ class Money
     @amount = amount
   end
 
-  def times(number)
-    self.class.new(amount * number)
+  def times(multiplier)
+    self.class.new(amount * multiplier)
   end
 
   def equal?(other)
     self.class == other.class && amount == other.amount
+  end
+
+  def self.dollar(amount)
+    Dollar.new(amount)
+  end
+
+  def self.franc(amount)
+    Franc.new(amount)
   end
 end
 
--- money_test.rb
+++ money_test.rb
@@ -5,21 +5,21 @@ require_relative 'money'
 
 class MoneyTest < Minitest::Test
   def test_muliplication
-    five = Dollar.new(5)
-    assert_same Dollar.new(10), five.times(2)
-    assert_same Dollar.new(15), five.times(3)
+    five = Money.dollar(5)
+    assert_same Money.dollar(10), five.times(2)
+    assert_same Money.dollar(15), five.times(3)
 
-    five = Franc.new(5)
-    assert_same Franc.new(10), five.times(2)
-    assert_same Franc.new(15), five.times(3)
+    five = Money.franc(5)
+    assert_same Money.franc(10), five.times(2)
+    assert_same Money.franc(15), five.times(3)
   end
 
   def test_equality
-    assert_same Dollar.new(5), Dollar.new(5)
-    refute_same Dollar.new(5), Dollar.new(6)
-    assert_same Franc.new(5), Franc.new(5)
-    refute_same Franc.new(5), Franc.new(6)
+    assert_same Money.dollar(5), Money.dollar(5)
+    refute_same Money.dollar(5), Money.dollar(6)
+    assert_same Money.franc(5), Money.franc(5)
+    refute_same Money.franc(5), Money.franc(6)
 
-    refute_same Franc.new(5), Dollar.new(5)
+    refute_same Money.franc(5), Money.dollar(5)
   end
 end
